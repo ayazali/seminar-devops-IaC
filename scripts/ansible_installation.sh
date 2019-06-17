0330d69a9e85efd9a2a66808504a4d1d299431bf
Ayaz Ali Qureshi <ayazaliqureshi@hotmail.com>
2019-06-17 10:54:56 +0200

    Adding env specific changes to the scripts

--- scripts/ansible_installation.sh
+++ scripts/ansible_installation.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install software-properties-common
+sudo apt --yes install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt --yes install ansible
+
+
+<< --MULTILINE-COMMENT--
+echo '192.168.222.26' | sudo tee -a /etc/ansible/hosts
+ssh-agent bash
+mkdir ../keys
+cd ../keys
+touch master.key
+ssh-add master.key
+--MULTILINE-COMMENT--
